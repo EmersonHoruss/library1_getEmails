afec0139e68d701fea7c69dbf308072bdf187675
Emersonhorus <peralesvillanuevaemerson@gmail.com>
2021-11-20 18:18:58 -0600

    'getEmailsCompetitor'

---
+++ server.js
@@ -0,0 +1,25 @@
+// two parameter: path and pathSave
+// path is the file's path
+// and pathSave is the file's path to save just with email data
+
+import {readFile,writeFile,open} from 'fs/promises'
+
+const path = './xd.txt'
+
+const bufferFile = await readFile(path)
+
+const jsonFile = JSON.parse(bufferFile)
+
+const emailFile = []
+
+jsonFile[0].forEach(e=>{
+
+	emailFile.push(e.email)
+
+})
+
+const pathSave = './xdSave.txt'
+
+await writeFile(pathSave,emailFile.toString())
+
+await open(pathSave)
